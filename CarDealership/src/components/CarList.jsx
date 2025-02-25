e79ffc53b9881d2f96337036637ccfd584fc759a
Nina Bucic <bucicnina@gmail.com>
2025-02-25 13:54:13 +0100

    Add red border for cars with registration expiring in less than 30 days

--- CarDealership/src/components/CarList.jsx
+++ CarDealership/src/components/CarList.jsx
@@ -6,23 +6,35 @@ function CarList({ cars, onDeleteCar }) {
       {cars.length === 0 ? (
         <h3 className="no-cars">No cars available.</h3>
       ) : (
-        cars.map((car) => (
-          <div key={car.id} className="car-card">
-            <h3>
-              {car.brand} {car.model}
-            </h3>
-            <p>
-              <strong>Type:</strong> {car.type}
-            </p>
-            <p>
-              <strong>Year:</strong> {car.year}
-            </p>
-            <p>
-              <strong>Exp. Reg:</strong> {car.registrationDate}
-            </p>
-            <button onClick={() => onDeleteCar(car.id)}>Remove</button>
-          </div>
-        ))
+        cars.map((car) => {
+          const regDate = new Date(car.registrationDate);
+          const timeDiff = regDate - new Date();
+          const daysLeft = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+          return (
+            <div
+              key={car.id}
+              className="car-card"
+              style={{
+                border: daysLeft <= 30 ? "3px solid red" : "1px solid #ccc",
+              }}
+            >
+              <h3>
+                {car.brand} {car.model}
+              </h3>
+              <p>
+                <strong>Type:</strong> {car.type}
+              </p>
+              <p>
+                <strong>Year:</strong> {car.year}
+              </p>
+              <p>
+                <strong>Exp. Reg:</strong> {car.registrationDate}
+              </p>
+              <button onClick={() => onDeleteCar(car.id)}>Remove</button>
+            </div>
+          );
+        })
       )}
     </div>
   );
